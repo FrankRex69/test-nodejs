c101d177e5700c20ad86e245d7ac7b66c874f584
Kj£99shjKu34qq <frare@publicisgroupe.net>
2023-05-08 16:43:44 +0200

    feat: page2

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Risposta server noode express !')
+  res.send('Risposta server node express !')
+})
+
+app.get('/page2', (req, res) => {
+  res.send('Risposta server node express da page 2 !')
 })
 
 app.listen(port, () => {
